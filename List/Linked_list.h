cbe560db969f2d592954db689de812983510deb2
minje46 <minjea1701@naver.com>
2019-10-30 19:27:59 +0900

    List
    Do implement the basic structure of list in c language.

---
+++ List/Linked_list.h
@@ -0,0 +1,14 @@
+#ifdef LINKED_LIST_H
+#define LINKED_LIST_H
+
+void push_front(int key);
+void push_back(int key);
+
+void pop_front();
+void pop_front();
+
+bool Empty();
+int Size();
+void Print();
+
+#endif // LINKED_LIST_H
