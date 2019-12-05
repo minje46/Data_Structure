6f0f70fe6c1e26104f41e4fcc1b17de3c990ba9e
minje46 <minjea1701@naver.com>
2019-12-04 18:00:11 -1200

    Stack / [Array]
    Do implement "stack" based on array.

---
+++ Stack/Implementation/Array/Stack.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifdef Stack_H
+
+void Push(int key);
+void Pop();
+
+int Peek();
+int Size();
+bool Empty();
+void Print();
+
+#endif // Stack_H
---
+++ Stack/Implementation/Array/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include "Stack.h"
+
+using namespace std;
+
+int main(void)
+{
+	
+}
