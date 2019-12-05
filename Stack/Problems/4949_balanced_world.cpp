8603f703c6e57203c12b76151de16b5f2e76aecc
minje46 <minjea1701@naver.com>
2019-12-05 20:37:13 +0900

    [acmicpc] balanced_world / [Stack]
    To figure out the pair of parentheses based on stack data structure.
    Explaination is attached as comment(annotation).

---
+++ Stack/Problems/4949_balanced_world.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+#include<stack>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);	cout.tie(0);
+
+	while (true)
+	{
+		string str = "";									    // Input variable.
+		getline(cin, str);
+
+		if (str.length() == 1 && str[0] == '.')		// End condition.
+			break;
+
+		bool flag = true;
+		stack<char> stk;
+		for (int i = 0; i < str.length(); i++)
+		{
+			if (str[i] == '(' || str[i] == '[')			// Parentheses.
+				stk.push(str[i]);
+			else if (str[i] == ')')
+			{
+				if (stk.empty() || stk.top() != '(')
+				{
+					flag = false;
+					break;
+				}
+				stk.pop();
+			}
+			else if (str[i] == ']')
+			{
+				if (stk.empty() || stk.top() != '[')
+				{
+					flag = false;
+					break;
+				}
+				stk.pop();
+			}
+		}
+
+		if (flag && stk.empty())						// Balanced string.
+			cout << "yes" << endl;
+		else											 		// Not pair of parentheses.
+			cout << "no" << endl;
+	}
+}
