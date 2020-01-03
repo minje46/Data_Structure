0cc27287e627c8af2201c6156813f3cbd6f8320c
minje46 <minjea1701@naver.com>
2020-01-04 02:24:57 +0900

    Stack / [List] in class
    Do implement "stack" in class based on list.

---
+++ Stack/Implementation/List/Class/main[List-Class].cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include "Stack[List-Class].h"
+using namespace std;
+
+Stack stk;							// Stack memory based on array.
+
+int main(void)
+{
+	if (stk.Empty())
+		cout << "Stack is empty" << endl;
+
+	for (int i = 1; i <= 10; i++)
+		stk.Push(i);
+
+	cout << "Size of stack : " << stk.Size() << endl;
+	stk.Print();
+
+	for (int i = 0; i < 5; i++)
+	{
+		stk.Pop();
+		cout << endl << "Size of stack : " << stk.Size() << endl;
+		stk.Print();
+	}
+}
