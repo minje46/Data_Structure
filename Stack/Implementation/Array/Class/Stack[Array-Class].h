4705ae22fd7d920e024f849eb8eea76df413b2ac
minje46 <minjea1701@naver.com>
2020-01-03 19:27:31 +0300

    Stack / [Class] / [Array]
    Do implement "stack" in class based on array.

---
+++ Stack/Implementation/Array/Class/Stack[Array-Class].h
@@ -0,0 +1,26 @@
+#ifndef _Array_Class_Stack_H		// if "_name.h" file is not defined,
+#define _Array_Class_Stack_H		// define "_name.h" file.	Then, do compile below code.
+
+#define MAX 1001
+
+class Stack
+{
+private:
+	int arr[MAX];
+	int top;
+
+public:
+	Stack();
+	~Stack();
+	bool Full();
+	bool Empty();
+	void Push(int key);
+	void Pop();
+	int Peek();
+	int Size();
+	void Print();
+};
+
+#else											// if not, do compile below code.
+
+#endif
