366a7952c45703a361eb96138e6db6ea53050874
minje46 <minjea1701@naver.com>
2020-01-08 16:29:44 +0900

    programmers_stock_price
    To figure out the stock price based on stack data structure.
    Explaination is attached as comment(annotation).

--- Stack/Problems/[Programmers]stock_price.cpp
+++ Stack/Problems/[Programmers]stock_price.cpp
@@ -9,23 +9,23 @@ vector<int> solution(vector<int> prices)
 	vector<int> answer(prices.size());
 	stack<int> stk;
 
-	for (int i = 0; i < prices.size(); i++)				// In the whole of prices.
+	for (int i = 0; i < prices.size(); i++)						// In the whole of prices.
 	{
 		while (!stk.empty() && prices[stk.top()] > prices[i])
-		{														// Compare the prices.	
-			answer[stk.top()] = i - stk.top();		// The moment when the price was decreased.
-			stk.pop();										// [Price가 감소한 시점에서, 자기 위치 index을 뺀 값이 유지된 시간.]
+		{																// Compare the prices.	
+			answer[stk.top()] = i - stk.top();				// The moment when the price was decreased.
+			stk.pop();												// [Price가 감소한 시점에서, 자기 위치 index을 뺀 값이 유지된 시간.]
 		}
-		stk.push(i);											// If the prices keep high, it is in the stack.
+		stk.push(i);													// If the prices keep high, it is in the stack.
 	}
 
-	while (!stk.empty())									// Remainders.
-	{															// [Price가 감소하지 않고 계속 증가한 경우들.]
+	while (!stk.empty())											// Remainders.
+	{																	// [Price가 감소하지 않고 계속 증가한 경우들.]
 		answer[stk.top()] = prices.size() - stk.top() - 1;
-		stk.pop();											// [전체 price들 중에서, 자기 위치 index와 1을 뺀 값이 유지된 시간.]	
+		stk.pop();													// [전체 price들 중에서, 자기 위치 index와 1을 뺀 값이 유지된 시간.]	
 	}
 
-	return answer;											// Output.
+	return answer;													// Output.
 }
 
 int main(void)
