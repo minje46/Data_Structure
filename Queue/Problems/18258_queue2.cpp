5aaa9402089f0ed084f7d7004ecaec98826fff16
minje46 <minjea1701@naver.com>
2020-01-19 23:13:03 +0900

    BASIC
    To use the STL <queue> data structure.
    Explaination is attached as comment(annotation).

---
+++ Queue/Problems/18258_queue2.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<queue>
+#include<cstring>
+
+using namespace std;
+
+queue<int> que;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);	cout.tie(0);
+
+	int test_case;
+	cin >> test_case;
+	cin.ignore();
+	for (int t = 1; t <= test_case; t++)
+	{
+		char ch[10];
+		cin >> ch;
+
+		if (!strcmp(ch, "push"))
+		{
+			int num;
+			cin >> num;
+			que.push(num);
+		}
+
+		else if (!strcmp(ch, "pop"))
+		{
+			if (que.empty())
+				cout << -1 << endl;
+			else
+			{
+				cout << que.front() << endl;
+				que.pop();
+			}
+		}
+
+		else if (!strcmp(ch, "size"))
+			cout << que.size() << endl;
+
+		else if (!strcmp(ch, "empty"))
+			cout << que.empty() << endl;
+
+		else if (!strcmp(ch, "front"))
+		{
+			if (que.empty())
+				cout << -1 << endl;
+			else
+				cout << que.front() << endl;
+		}
+
+		else if (!strcmp(ch, "back"))
+		{
+			if (que.empty())
+				cout << -1 << endl;
+			else
+				cout << que.back() << endl;
+		}
+	}
+}
