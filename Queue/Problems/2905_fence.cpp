29c8e2f89500e3eb2d95c45bd98c916b1cacb4c1
minje46 <minjea1701@naver.com>
2020-01-20 22:14:18 -1200

    Monotonic Priority Queue
    To figure out the minimum number of rollers based on monotonic priority queue data structure.
    Explaination is attached as comment(annotation).

--- Queue/Problems/2905_fence.cpp
+++ Queue/Problems/2905_fence.cpp
@@ -1,40 +1,55 @@
 #include<iostream>
-#include<algorithm>
-#include<memory.h>
-#include<vector>
-#include<queue>
+#include<deque>
+
+#define MAX 10000001
 
 using namespace std;
 
-int N, X;								// N = The number of fences.				X = The size of rollers.
-vector<int> fence;				// fence = The memory of input data.
-int cnt = 0, res = 0;
+int N, X;											// N = The number of fences.				X = The size of rollers.
+int fence[MAX];								// fence = The memory of input data.
+long long area = 0;							// area = The size of area to paint.
+int cnt = 0;										// cnt = The number of rollers.
 
-void Simulation()
+void Simulation()			// To do based on monotonic priority queue algorithm.
 {
-	vector<int> ans(N, 0);
-	for (int i = 0; i < N; i += X)
-	{
-		if (i + X >= N)
-			i = N - X;
-		queue<int> que;
-		cnt += 1;
-		for (int j = i; j < i + X; j++)
-			que.push(fence[j]);
-		
-		int mn = 987654321;
-		while (!que.empty())
-		{
-			mn = min(mn, que.front());
-			que.pop();
-		}
-
-		for (int j = i; j < i + X; j++)
-			ans[j] = mn;
+	deque<pair<int, int>> deq;			// pair = {idx, fence_height}.
+	for (int i = 0; i < X; i++)
+	{												// [minimum height를 deque의 front에 배치.]
+		while (!deq.empty() && deq.back().second > fence[i])
+			deq.pop_back();
+		deq.push_back(make_pair(i, fence[i]));
 	}
 
-	for (int i = 0; i < N; i++)
-		res += (fence[i] - ans[i]);
+	int height = deq.front().second;		// height = The height to paint.
+	int idx = 0;
+	for (int i = X; i <= N; i++)
+	{												// Monotonic priority queue.
+		while (!deq.empty() && deq.back().second > fence[i])
+			deq.pop_back();
+		deq.push_back(make_pair(i, fence[i]));
+													// Compare the minimum height to paint.
+		if (height != deq.front().second)
+		{											// [초기 상태 (0~X) 이후, 새로운 minimum height을 찾은 경우,			
+			cnt += (i - idx - 1) / X + 1;	// area, cnt 계산.]
+			area -= (long long)(i - idx) *height;
+			idx = i;
+			height = deq.front().second;
+		}
+													// Check the index to avoid overflow.
+		if (deq.front().first <= i - X)
+		{
+			int cur = deq.front().first;
+			deq.pop_front();
+													// [minimum height가 유지되는 경우,
+			if (height != deq.front().second)		// area, cnt 계산을 위함.]
+			{
+				cnt += (cur - idx) / X + 1;
+				area -= (long long)(cur - idx + 1)*height;
+				idx = cur + 1;
+				height = deq.front().second;
+			}
+		}
+	}
 }
 
 int main(void)
@@ -45,13 +60,12 @@ int main(void)
 	cin >> N >> X;
 	for (int i = 0; i < N; i++)
 	{
-		int num;
-		cin >> num;
-		fence.push_back(num);
+		cin >> fence[i];
+		area += fence[i];
 	}
 
 	Simulation();
 
-	cout << res << endl;
+	cout << area << endl;
 	cout << cnt << endl;
 }
