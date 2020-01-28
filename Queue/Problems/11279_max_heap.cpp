3263f976025070564e42f5247be591eb2d70a8d7
minje46 <minjea1701@naver.com>
2020-01-28 22:58:34 +0900

    BASIC_PRIORITY_QUEUE
    To use the STL <priority_queue> data structure.
    Explaination is attached as comment(annotation).

--- Queue/Problems/11279_max_heap.cpp
+++ Queue/Problems/11279_max_heap.cpp
@@ -3,14 +3,14 @@
 
 using namespace std;
 
-priority_queue<int> que;			// que = The memory of max heap.
+priority_queue<int> que;				// que = The memory of max heap.
 
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);	cout.tie(0);
 
-	int N;									// N = The number of instructions.
+	int N;										// N = The number of instructions.
 	cin >> N;
 	for (int i = 0; i < N; i++)
 	{
